60f63f06a8d43da0b84c556a83a0ad3c5598b8f7
Numan Ahmed <mdnmnahmed@gmail.com>
2021-04-27 08:40:45 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import {useEffect} from 'react';
 import Body from './Components/Body/Body';
+import HeaderComponent from './Components/Header/HeaderComponent';
+import TopSectionComponent from './Components/TopSection/TopSectionComponent';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './styles/materialize.css';
+import './styles/style.css';
+import './styles/custom.css';
+import Users from './Components/Body/Users/Users';
 
 const App = () => {
+
+    useEffect(() => {
+        // Init Materialize JS
+        M.AutoInit();
+    });
+
     return (
-        <div>
-            <Body />
-        </div>
+        <>
+            <HeaderComponent />
+            <TopSectionComponent />
+            <Users />
+        </>
     );
 }
 
