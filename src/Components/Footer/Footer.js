0f4d9fdc6db38aadfd2beee07f83fde45efd7f1b
Numan Ahmed <mdnmnahmed@gmail.com>
2021-05-04 17:12:24 +0530

    Footer Created

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div align="center">
+                <h5>API's-Store © 2K21</h5>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
